10035b1a2c1a1fb541408e837d03815e6b249357
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2026-06-15 19:12:03 -0300

    feat: adicionado audio service interface e audio mixer assets

---
+++ Assets/_Game/Core/Services/IAudioService.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+namespace Seventh.Core.Services
+{
+    public interface IAudioService
+    {
+        // ============ VOLUMES ============
+        float MusicVolume { get; }
+        float SfxVolume { get; }
+        float AmbientVolume { get; }
+
+        // ============ MIXER PARAMETERS ============
+        const string MUSIC_MIXER_PARAMETER = "MusicVolume";
+        const string SFX_MIXER_PARAMETER = "SFXVolume";
+        const string AMBIENT_MIXER_PARAMETER = "AmbientVolume";
+
+        // ============ TRACK ============
+        void PlayTrack(string trackName, AudioSettings settings);
+        void PlayTrack(AudioClip clip, AudioSettings settings);
+        void StopTrack(string trackName);
+        void StopTrack(AudioClip clip);
+
+        // ============ SFX ============
+        void PlaySFX(string sfxName, AudioSettings settings);
+        void PlaySFX(AudioClip clip, AudioSettings settings);
+        void StopSFX(string sfxName);
+        void StopSFX(AudioClip clip);
+
+        // ============ AMBIENT ============
+        void PlayAmbient(string ambientName, AudioSettings settings);
+        void PlayAmbient(AudioClip clip, AudioSettings settings);
+        void StopAmbient(string ambientName);
+        void StopAmbient(AudioClip clip);
+
+        // ============ VOLUME CONTROL ============
+        void SetMusicVolume(float volume);
+        void SetSFXVolume(float volume);
+        void SetAmbientVolume(float volume);
+    }
+
+    public struct AudioSettings
+    {
+        public float Pitch; // Valor de pitch para a reprodução do áudio
+        public float VolumeOffset; // ajuste relativo aplicado sobre o volume base de um som, em vez de definir um valor absoluto.
+        public bool Loop; // Indica se o áudio deve ser reproduzido em loop
+
+        public AudioSettings(float pitch = 1f, float volumeOffset = 0f, bool loop = false)
+        {
+            Pitch = pitch;
+            VolumeOffset = volumeOffset;
+            Loop = loop;
+        }
+    }
+}
